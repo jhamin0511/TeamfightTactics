bc566c2396ec92b747d613ca2f49e5dcd7e69cb0
jhamin0511 <jhamin0511@gmail.com>
2020-06-28 13:45:25 +0900

    chore: Koin 라이브러리 추가
    "org.koin:koin-android:2.1.6""
    "org.koin:koin-android-scope:2.1.6""
    "org.koin:koin-android-viewmodel:2.1.6""
    "org.koin:koin-test:2.1.6""

--- app/src/main/java/com/github/jhamin0511/teamfighttactics/application/TeamfightTacticsApplication.kt
+++ app/src/main/java/com/github/jhamin0511/teamfighttactics/application/TeamfightTacticsApplication.kt
@@ -2,6 +2,11 @@ package com.github.jhamin0511.teamfighttactics.application
 
 import android.app.Application
 import com.github.jhamin0511.teamfighttactics.BuildConfig
+import com.github.jhamin0511.teamfighttactics.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
@@ -10,7 +15,20 @@ class TeamfightTacticsApplication : Application() {
   override fun onCreate() {
     super.onCreate()
 
+    initKoin()
     initTimber()
+  }
+
+  private fun initKoin() {
+    val modules: List<Module> = listOf(
+      appModule
+    )
+
+    startKoin {
+      androidContext(this@TeamfightTacticsApplication)
+      androidLogger()
+      modules(modules)
+    }
   }
 
   private fun initTimber() {
